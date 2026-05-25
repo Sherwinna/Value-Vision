cf7d47b157fa9a9d5ae760a0175ebd4aa6022652
Sherwinna <sherwinc04@gmail.com>
2026-05-25 16:26:37 +0800

    update frontend to use deployed backend URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function StockTable() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/api/stocks")
+    fetch("https://value-vision-backend.onrender.com/api/stocks")
       .then(res => res.json())
       .then(data => {
         setStocks(data)
