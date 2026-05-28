54c047808c9d397aaa805b32fb19fa220ae70aa2
Sherwinna <sherwinc04@gmail.com>
2026-05-28 14:05:32 +0800

    fix api url to use deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:5000"
+const API =  "https://value-vision-backend.onrender.com"
 
 export async function fetchWithAuth(url, options = {}) {
   const token = localStorage.getItem("token")
